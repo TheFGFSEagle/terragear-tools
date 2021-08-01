e1308d2eb7d379052d680c296a6bdc1850deb2c0
Frederic <programmer.anonymous@yandex.com>
2021-08-01 18:11:51 +0200

    Added README.md content and finished process-elevations.py script

--- process-elevations.py
+++ process-elevations.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
+
+# Small wrapper script for gdalchop that, if you are following the standar TerraGear directory structure,
+# can be run without arguments
 
 import sys
-import os
 import argparse
 import subprocess
 
+argp = argparse.ArgumentParser(description="process-elevations.py - process a directory of elevation data files with gdalchop and terrafit")
+
+argp.add_argument(
+	"-i", "--input-folder",
+	help="folder containing the raw elevation files (default: %(default)s)",
+	default="./data/elevation",
+	metavar="INPUT",
+)
+
+argp.add_argument(
+	"-o", "--output-folder",
+	help="where to put the produced files (default: %(default)s)",
+	default="./work/elevation",
+	metavar="OUTPUT"
+)
+
+args = argp.parse_args()
+
+chop = subprocess.Popen(["gdalchop", args.output_folder, args.input_folder], stdout=sys.stdout, stderr=sys.stderr)
+exit = chop.run()
+sys.exit(exit)
 
